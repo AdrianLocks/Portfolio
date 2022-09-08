958cb40f57ff0c2c1e42b74b99fda0cc4bd835ad
AdrianLocks <93447997+AdrianLocks@users.noreply.github.com>
2022-09-08 16:10:01 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'); /* constantes para o navbar e menu */
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener('click', () => {    /* evento de clique dos menus */
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
 });
 
-document.addEventListener('scroll', () => {
+document.addEventListener('scroll', () => {     /* evento de rolagem para o navbar mudar a transparencia */
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
 		header.style.backgroundColor = '#29323c';
@@ -17,7 +17,7 @@ document.addEventListener('scroll', () => {
 	}
 });
 
-menu_item.forEach((item) => {
+menu_item.forEach((item) => {               /* evento para a troca de menus dekstop e mobile */
 	item.addEventListener('click', () => {
 		hamburger.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
