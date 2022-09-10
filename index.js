9a4d02cfd135cb099ab38a2fa303fe918d57c511
AdrianLocks <93447997+AdrianLocks@users.noreply.github.com>
2022-09-10 03:12:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const $html = document.querySelector('html')
 const $checkbox = document.querySelector('#switch')
 
-$checkbox.addEventListener('change',function(){
+$checkbox.addEventListener('change',function(){      /* evento checkbox mudar as cores */
     $html.classList.toggle('ligth')
 })
