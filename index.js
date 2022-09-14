92a3b1737ea7476afb410dcbdebd21960dd0b068
AdrianLocks <93447997+AdrianLocks@users.noreply.github.com>
2022-09-13 21:56:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const $html = document.querySelector('html')
-const $checkbox = document.querySelector('#switch')
+const $checkbox = document.querySelector('#flexSwitchCheckDefault')
 
 $checkbox.addEventListener('change',function(){      /* evento checkbox mudar as cores */
     $html.classList.toggle('ligth')
